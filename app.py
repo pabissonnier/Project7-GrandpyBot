cd5e8da14a5b30e75e7c96330407bce8d68ff997
pabissonnier <pabissonnier@gmail.com>
2019-04-10 10:18:32 +0200

    Adding some style to css to welcome 3 parts of the answers

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect, request, jsonify
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 app = Flask(__name__)
 
 
@@ -10,11 +12,18 @@ def home():
     return render_template('index.html', title="Bienvenue chez GrandPy Bot")
 
 
-# Parssing à intégrer en dessous
 @app.route('/_answer')
 def answer():
     question = request.args.get('question', 0, type=str)
-    return jsonify(result=question)
+    question = question.lower()
+
+    stop_words = stopwords.words('french')
+
+    word_tokens = question.split()
+
+    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+
+    return jsonify(result=filtered_sentence)
 
 
 if __name__ == '__main__':
